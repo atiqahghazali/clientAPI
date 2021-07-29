445fb46b59e0ecf416d3d2d446e119e707615076
Atiqah Ghazali <atiqahghazali09@gmail.com>
2021-07-29 17:46:51 +0800

    add token bearer in header

--- app/Http/Controllers/API/HouseController.php
+++ app/Http/Controllers/API/HouseController.php
@@ -16,8 +16,11 @@ class HouseController extends Controller
     public function index()
     {
         // tarik api
-    $url = 'http://127.0.0.1:8000/api/houses';
-    $response = Http::get($url);
+    $url = 'http://127.0.0.1:8000/api/v1/houses';
+    $response = Http::withHeaders([
+        'Accept' => 'application/json',
+        'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+    ])->get($url);
     $houses = $response->json()['data']['data'];
     // dd($houses['data']);
     return view('house.index', compact('houses'));
@@ -41,9 +44,10 @@ class HouseController extends Controller
      */
     public function store(Request $request)
     {
-        $url = 'http://127.0.0.1:8000/api/houses/create';
+        $url = 'http://127.0.0.1:8000/api/v1/houses/create';
         $response = Http::withHeaders([
             'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
         ])->post($url, 
         [
             'type' => $request->type,
@@ -72,8 +76,11 @@ class HouseController extends Controller
      */
     public function edit($id)
     {
-        $url = 'http://127.0.0.1:8000/api/houses/edit/'.$id;
-        $response = Http::get($url);
+        $url = 'http://127.0.0.1:8000/api/v1/houses/edit/'.$id;
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+        ])->get($url);
         // dd($response->object()->data);
         $house = $response->object()->data;
         return view('house.edit',compact('house'));
@@ -88,8 +95,11 @@ class HouseController extends Controller
      */
     public function update(Request $request, $id)
     {
-        $url = 'http://127.0.0.1:8000/api/houses/update/'.$id;
-        $response = Http::post($url, [
+        $url = 'http://127.0.0.1:8000/api/v1/houses/update/'.$id;
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+        ])->post($url, [
             'type' => $request->type,
             'price' => $request->price
         ]);
@@ -105,8 +115,11 @@ class HouseController extends Controller
      */
     public function destroy($id)
     {
-        $url = 'http://127.0.0.1:8000/api/houses/delete/'.$id;
-        $response = Http::delete($url);
+        $url = 'http://127.0.0.1:8000/api/v1/houses/delete/'.$id;
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+        ])->delete($url);
         // dd($response->object()->data);
         return redirect()->route('house-index');
     }
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -12,13 +12,24 @@ class UserController extends Controller
     public function index(Request $request)
     {
         // tarik api
-    $url = 'http://127.0.0.1:8000/api/users';
-    $response = Http::get($url,[
+    $url = 'http://127.0.0.1:8000/api/v1/users';
+    $response = Http::withHeaders([
+        'Accept' => 'application/json',
+        'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+    ])->get($url,[
         'search' => $request->search
     ]);
-    $users = $response->json()['data']['data'];
-    // dd($users['data']);
-    return view('user.index', compact('users'));
+
+    if($response->json()['message'] == 'Unauthenticated.'){
+        return $response->json()['message'];
+    }elseif($response->json()['success'] == true){
+        $users = $response->json()['data']['data'];
+
+        return view('user.index', compact('users'))->with([
+        'alert-type' => 'alert-success',
+        'alert-message' => $response->json()['message']
+        ]);
+    }
     }
 
     public function create()
@@ -28,55 +39,81 @@ class UserController extends Controller
 
     public function store(Request $request)
     {
-        $url = 'http://127.0.0.1:8000/api/users/create';
+        $url = 'http://127.0.0.1:8000/api/v1/users/create';
         $response = Http::withHeaders([
             'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
         ])->post($url, [
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password
         ]);
-        dd($response->object()->errors);
-        if($response->successful()){
-            return redirect()->route('user-index')->with([
+        dd($response->json()['message']);
+        if($response->json()['message'] == 'Unauthenticated.'){
+            return $response->json()['message'];
+        }elseif($response->json()['message'] == 'The given data was invalid.'){
+            return redirect()->back()->with([
                 'alert-type' => 'alert-success',
-                'alert-message' => 'User has been successfuly added'
+                'alert-message' => $response->json()['message']
             ]);
         }else{
             return redirect()->route('user-index')->with([
-                'alert-type' => 'alert-danger',
-                'alert-message' => 'failed'
+                'alert-type' => 'alert-success',
+                'alert-message' => $response->json()['message']
             ]);
         }
         
     }
 
     public function show($id){
-        $url = 'http://127.0.0.1:8000/api/users/'.$id;
-        $response = Http::get($url);
+        $url = 'http://127.0.0.1:8000/api/v1/users/'.$id;
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+        ])->get($url);
         // dd($response->object()->data);
-        $user = $response->object()->data;
-        return view('user.edit',compact('user'));
+        if($response->json()['message'] == 'Unauthenticated.'){
+            return $response->json()['message'];
+        }else{
+            $user = $response->object()->data;
+            return view('user.edit',compact('user'));
+        }
+        
     }
 
     public function update(Request $request, $id)
     {
-        $url = 'http://127.0.0.1:8000/api/users/update/'.$id;
-        $response = Http::post($url, 
+        $url = 'http://127.0.0.1:8000/api/v1/users/update/'.$id;
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+        ])->post($url, 
         [
             'name' => $request->name,
             'email' => $request->email,
             'password' => $request->password
         ]);
 
-        return redirect()->route('user-index');
+        if($response->json()['message'] == 'Unauthenticated.'){
+            return $response->json()['message'];
+        }else{
+            return redirect()->route('user-index');
+        }
+        
     }
 
     public function destroy($id)
     {
-        $url = 'http://127.0.0.1:8000/api/users/delete/'.$id;
-        $response = Http::delete($url);
-        // dd($response->object()->data);
-        return redirect()->route('user-index');
+        $url = 'http://127.0.0.1:8000/api/v1/users/delete/'.$id;
+        $response = Http::withHeaders([
+            'Accept' => 'application/json',
+            'Authorization' => 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMTliZGI1YzU4NTQ4ZjAxNTU3ZGUyYTBmZDcwYWQwMzdjMjRiZjJhYTk2MTYwOGE1OTNkYjA5MGYyNTlmYjgwOGNhZTZjNDc4NDEzMTVhMzciLCJpYXQiOjE2Mjc1NDg1NDAuNzkxMDcsIm5iZiI6MTYyNzU0ODU0MC43OTEwNzIsImV4cCI6MTY1OTA4NDU0MC43ODk5ODIsInN1YiI6IjEiLCJzY29wZXMiOltdfQ.VfjQpkA5DYo6-eeYQubuE5cs60ytmhF9RvEopzKvziAZlEHH6hm2hasaF33afD-9LJUBINDtcMYrijJ1P2JRtiJFbUhkUv_QQEqcpQHXf-FGW_v34vRJLcB8iXdiI56HEdAOwU7Nl9OBee0rrVQteDTUBLZsHbDvnh8E4wCDtxPALU_5F8J1N36NkuHWImrC98W_7AQiOx8SbNU1bJ7HUi9BgRFoSVMFN1yJgOZfuTbEqnAXZc0Pmmej4zuJDv0-w_Hl2AfTmCT8p5AxyXiyN35JwTmaY_4S4y3TwLAKIYhOt_kWbrOFnLwCB5bcYuATO6eaGMC1m4yjF6ZhL6TARcWq_WJYODZgeRLlfhP1OgEApRnXRxuQnzuoASuDOGzq1_IEn3QZ1v7fVNjaHX-JDk8iHQUDMb7vCUquIca__eFEAbp5ByNK9Za5kpDJJygugW0bZd8DxnynG-Z2QyRr5bRQSckwhEfhGfwIw0Ew_ZAeoALr5eyJtB7NubddxijgXJRiLD_AUvku0IAFUdTYiAucjU8tEkTKVH9KghQ7JJJjpjf2j9_4Vlnh7Xjthc0UTnVMcreA_1v6aDwS-rbTZNgEFTgofWUIm0z2Z6U1Pcp_80ynoQWpCuF4tcPDVlPe4E9Vl1CDxEvOMG5aqjlvvz0IoXsM9UGoEzsMcmG3WLY'
+        ])->delete($url);
+
+        if($response->json()['message'] == 'Unauthenticated.'){
+            return $response->json()['message'];
+        }else{
+            return redirect()->route('user-index');
+        }
     }
 }
